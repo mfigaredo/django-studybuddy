6ed5cf8e43c604e7158e54315351265d78273136
Miguel Figaredo <mfigaredo@gmail.com>
2023-03-11 19:51:57 -0600

    Deployment Try #4

--- studybud/settings.py
+++ studybud/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # if DEBUG:
 #     STATICFILES_DIRS = [
@@ -152,12 +153,10 @@ STATIC_URL = '/static/'
 #     ]
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-if DEBUG or True:    
-    MEDIA_URL = 'images/'
-    MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'media/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
